3aab7a3db54efc4a6460d06e8da45add835ecf42
Yogi <bugracelenk01@gmail.com>
2020-01-06 01:45:40 +0300

    fixed stores route

--- routes/stores.js
+++ routes/stores.js
@@ -6,7 +6,7 @@ const checkAuth = require("../middleware/checkAuth");
 const isStoreUser = require("../middleware/isStoreUser");
 const StoreControllers = require("../controllers/stores");
 
-router.get('/get-store', checkAuth, isStoreUser, StoreControllers.get_store);
+router.get('/', checkAuth, isStoreUser, StoreControllers.get_store);
 router.get('/:profile_id', StoreControllers.get_store_by_id);
 router.patch('/update-store', checkAuth, isStoreUser, StoreControllers.update_store);
 router.delete('/delete-store', checkAuth, isStoreUser, StoreControllers.update_store);
